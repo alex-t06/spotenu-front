752edb51234e7a1e97ca2e20021c84da515e2356
Alex <alex.trindade@live.com>
2020-09-03 00:08:43 -0300

    início de projeto e criação da homePage

---
+++ spotenu/src/pages/HomePage/StyleHomePage.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ContainerHome = styled.section`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(to top, #121212 8%, #fff952 120%);
+
+  img {
+    width: 100%;
+    height: 50%;
+  }
+`;
+
+export const StyledButton = {
+  backgroundColor: "#121212",
+  borderRadius: 25,
+  color: "#fff952",
+  fontSize: 20,
+  fontWeight: 900,
+  height: 60,
+  marginTop: 30,
+};
---
+++ spotenu/src/pages/ListenerRegistration/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListenerRegistration = () => {
+  return <div>ListenerRegistration</div>;
+};
+
+export default ListenerRegistration;
